0f321ed752f8ca2537774e70096aab488f942369
va1entin <valh@mailbox.org>
2019-11-10 16:27:40 +0100

    initial commit of configs and scripts, update README

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # HomeServerPi
 
-This repository contains:
+This repository contains configs, scripts etc. from my small home server, which currently is a Raspberry Pi 4B with 4GB RAM.
 
 * Ansible playbooks
 * configs
-* and more
 
-from my small home server, which currently is a Raspberry Pi 4B with 4GB RAM.
+* scripts
+
+**openvpn/cloudflare-update-record.sh**
+
+*Forked from [benkulbertis](https://gist.github.com/benkulbertis/fff10759c2391b6618dd)*
+
+Updates a cloudflare DNS record with the systems current external IPv4 address.
+
---
+++ openvpn/cloudflare-update-record.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+# Forked from https://gist.github.com/benkulbertis/fff10759c2391b6618dd
+
+#set -x
+
+# These API tokens are required
+
+# READ TOKEN permissions:
+# Zone.DNS
+# EDIT TOKEN permissions:
+# Account.Access: Organizations, Identity Providers, and Groups, Zone.Zone Settings, Zone.Zone, Zone.DNS
+
+read_token="YOUR READ TOKEN"
+edit_token="YOUR EDIT TOKEN"
+zone_name="domain.tld"
+record_name="sub.domain.tld"
+
+# MAYBE CHANGE THESE
+ip=$(curl -s http://ipv4.icanhazip.com)
+ip_file="ip.txt"
+id_file="cloudflare.ids"
+log_file="cloudflare.log"
+
+# LOGGER
+log() {
+    if [ "$1" ]; then
+        echo -e "[$(date)] - $1" >> $log_file
+    fi
+}
+
+# SCRIPT START
+log "Check Initiated"
+
+if [ -f $ip_file ]; then
+    old_ip=$(cat $ip_file)
+    if [ $ip == $old_ip ]; then
+        echo "IP has not changed."
+        exit 0
+    fi
+fi
+
+if [ -f $id_file ] && [ $(wc -l $id_file | cut -d " " -f 1) == 2 ]; then
+    zone_identifier=$(head -1 $id_file)
+    record_identifier=$(tail -1 $id_file)
+else
+    zone_identifier=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$zone_name" -H "Authorization: Bearer $read_token" -H "Content-Type: application/json" | grep -Po '(?<="id":")[^"]*' | head -1 )
+    record_identifier=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records?name=$record_name" -H "Authorization: Bearer $read_token" -H "Content-Type: application/json"  | grep -Po '(?<="id":")[^"]*')
+    echo "$zone_identifier" > $id_file
+    echo "$record_identifier" >> $id_file
+fi
+
+update=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records/$record_identifier" -H "Authorization: Bearer $edit_token" -H "Content-Type: application/json" --data "{\"id\":\"$zone_identifier\",\"type\":\"A\",\"name\":\"$record_name\",\"content\":\"$ip\"}")
+
+if [[ $update == *"\"success\":false"* ]]; then
+    message="API UPDATE FAILED. DUMPING RESULTS:\n$update"
+    log "$message"
+    echo -e "$message"
+    exit 1
+else
+    message="IP changed to: $ip"
+    echo "$ip" > $ip_file
+    log "$message"
+    echo "$message"
+fi
